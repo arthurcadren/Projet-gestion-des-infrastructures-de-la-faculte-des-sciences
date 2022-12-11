65c1a1c09dc088d7f1051c65c07de52d8792fedd
arthur <cadrenarthur@gmail.com>
2022-12-11 16:19:06 +0000

    initialisation du projet

---
+++ resources/views/page1.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">tous va s en co ui lle</div>
+
+    <script type="text/javascript" src="{{asset('js/app.jss')}}"></script>
+</body>
+</html>
